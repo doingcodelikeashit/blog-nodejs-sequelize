5a682a03accfcdd009482e66b3fba8bb43d5d586
doingcodelikeashit <doingcodelikeashit@gmail.com>
2024-12-12 11:05:25 +0530

    package installation and server setup at port 8008

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const port = process.env.PORT;
+app.use(express.json());
+app.listen(port, () => console.log(`Server started on ${port}!`));
